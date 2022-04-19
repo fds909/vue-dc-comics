9414d1fc7e06402279cc92eeceb6f63bf2eea370
Francesco De Sìmini <francescod995@gmail.com>
2022-04-19 15:32:51 +0200

    fatto Jumbotron

---
+++ src/components/CurrentSeriesComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="current-series">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CurrentSeriesComp',
+    }
+</script>
+
+<style scoped lang="scss">
+    .current-series {
+        padding: 30px 0;
+    }
+</style>
